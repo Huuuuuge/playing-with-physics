4297a3c468b02d61dc86a4b59722eb304a9e542d
sapienhomo <105915520+Huuuuuge@users.noreply.github.com>
2023-01-05 22:43:27 +0530

    initial

---
+++ Assets/collision.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+
+
+using UnityEngine;
+
+public class collision : MonoBehaviour
+
+{
+    private void OnCollisionEnter(Collision collision)
+    {
+
+
+
+
+        Color randomlySelectedColor = GetRandomColor();
+        GetComponent<Renderer>().material.color = randomlySelectedColor;
+    }
+
+    private Color GetRandomColor()
+
+    {
+        return new Color(
+        r: UnityEngine.Random.Range(0f, 1f),
+        g: UnityEngine.Random.Range(0f, 1f),
+        b: UnityEngine.Random.Range(0f, 1f));
+    }
+}
